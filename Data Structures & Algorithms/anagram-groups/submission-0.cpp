a38d7a861486c3f20d12b01a5c64334979eaa0e3
Reacher3745 <mainhoongarv@gmail.com>
2026-08-13 03:36:42 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+        for (string s : strs) {
+            string a = s;
+            sort(a.begin(), a.end());
+            mp[a].push_back(s);
+        }
+
+        for(auto it : mp) {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+
+};
